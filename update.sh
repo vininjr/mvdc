05e9f40823dc20b7e5b9bc887e131282f69a4771
Marcus Vinicius <viniips@alu.ufc.br>
2019-05-12 13:14:18 -0300

    update file update.sh to correct install after update storege

--- update.sh
+++ update.sh
@@ -8,6 +8,7 @@ fi
 if [ -d ".git" ]; then
     git pull
     git submodule update --init --recursive --remote
+    sudo chmod +x install.sh
     sudo ./install.sh
 else
     echo "Not a git repository (or any of the parent directories): .git. Do NOT download the repository as a zip file from GitHub.com! Please download Fuzion by cloning the Git repository: 'git clone --recursive https://github.com/vininjr/mvdc.git'"
